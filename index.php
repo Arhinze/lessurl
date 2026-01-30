9d2849ef686fc828a567210789bd1201e7eb2dcd
Arinze <francisokoye48@gmail.com>
2026-01-30 03:30:35 +0100

    properly branding LessURL

--- index.php
+++ index.php
@@ -7,7 +7,6 @@ include_once($_SERVER["DOCUMENT_ROOT"]."/php/account-manager.php");
 // 1. HANDLE REDIRECTION
 if (isset($_GET['url_path'])) {
     $path = htmlentities(str_replace("/","",$_GET['url_path']));
-    //$path = parse_url($url, PHP_URL_PATH);
     $link_stmt = $pdo->prepare("SELECT * FROM links WHERE custom_name = ?");
     $link_stmt->execute([$path]);
     $link_data = $link_stmt->fetch(PDO::FETCH_OBJ);
